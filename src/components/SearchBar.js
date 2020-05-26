6e2eb3be17ba64a5c00e655cc0cc56eaa820be9d
Lalama1982 <lalamaesp@gmail.com>
2020-05-26 22:02:08 +1000

    App Start and Search Input Text

---
+++ src/components/SearchBar.js
@@ -0,0 +1,45 @@
+import React from "react";
+import { View, TextInput, StyleSheet } from "react-native";
+import { Feather } from "@expo/vector-icons"; // importing icon lib "Feather" from "expo"
+
+const SearchBar = ({ term, onTermChange, onTermSubmit }) => {
+  return (
+    <View style={styles.backgroundStyle}>
+      <Feather name="search" style={styles.iconStyle} />
+      <TextInput
+        autoCapitalize="none"
+        autoCorrect={false}
+        style={styles.inputStyle}
+        placeholder="Search"
+        value={term}
+        //onChangeText={(newTerm) => onTermChange(newTerm)}
+        onChangeText={onTermChange} // just passing a reference to the function. here "newTerm" object is passed implicitly as of the definition
+        onEndEditing={() => onTermSubmit()} // to be effective of "Ok" or Enter button of the keyboard
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  backgroundStyle: {
+    marginTop: 10,
+    backgroundColor: "#F0EEEE",
+    height: 50,
+    borderRadius: 5,
+    marginHorizontal: 15,
+    flexDirection: "row",
+  },
+  inputStyle: {
+    //borderColor: "black",
+    //borderWidth: 1,
+    flex: 1,
+    fontSize: 18,
+  },
+  iconStyle: {
+    fontSize: 35,
+    alignSelf: "center",
+    marginHorizontal: 15,
+  },
+});
+
+export default SearchBar;
